f2e538eaa49cae77e6f8753c988d561c011e2f76
c0dethink <c0de-think@protonmail.com>
2025-02-05 09:50:13 +0800

    file: outputStringLengthtest

---
+++ src/com/c0dethink/outputStringLength.java
@@ -0,0 +1,12 @@
+package com.c0dethink;
+
+public class outputStringLength {
+    public static void main(String[] args) {
+        String string = "Hello World";
+        System.out.println(string.length());
+        String str = "Hello";
+        str = str.concat(" World");
+        System.out.println(str);
+        char ch = '1';
+    }
+}
