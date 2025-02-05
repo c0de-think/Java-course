af5c4c72df6e3be006e037a03e7f1388c2ebe996
c0dethink <c0de-think@protonmail.com>
2025-02-05 09:52:39 +0800

    exercise: terminal run java

---
+++ src/com/c0dethink/Exercise.java
@@ -0,0 +1,7 @@
+package com.c0dethink;
+
+public class Exercise {
+    public static void main(String[] args) {
+        System.out.println("I can compile and run from the terminal. Hooray 🎉");
+    }
+}
