bbcc54a7d4dbc76d9be596eeaa75f6e317ea7369
c0dethink <c0de-think@protonmail.com>
2025-02-05 16:15:00 +0800

    git for professionals course

---
+++ src/com/professionals/loopsAndArrays.java
@@ -0,0 +1,12 @@
+package com.professionals;
+
+public class loopsAndArrays {
+    public static void main(String[] args) {
+//        create an array
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+//        use index for loop
+        for (int i = 0; i < 10; i++) {
+            System.out.println(numbers[i]);
+        }
+    }
+}
