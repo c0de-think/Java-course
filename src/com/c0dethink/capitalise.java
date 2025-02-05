3dcfeaead8d36054126b6a636422886c7c24de21
c0dethink <c0de-think@protonmail.com>
2025-02-05 09:56:12 +0800

    stringName.toUpperCase()

---
+++ src/com/c0dethink/capitalise.java
@@ -0,0 +1,9 @@
+package com.c0dethink;
+
+public class capitalise {
+    public static void main(String[] args) {
+        String before = "hello";
+        String after = before.toUpperCase();
+        System.out.println(after);
+    }
+}
