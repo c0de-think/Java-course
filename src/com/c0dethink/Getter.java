33d543b060324ff37fac98b0004f0939507a52cc
c0dethink <c0de-think@protonmail.com>
2025-02-06 17:55:01 +0800

    Setter&Getter

---
+++ src/com/c0dethink/Getter.java
@@ -0,0 +1,13 @@
+package com.c0dethink;
+
+public class Getter {
+    private String name;
+
+    public void SetName(String name) {
+        this.name = name;
+    }
+
+    public String GetName() {
+        return this.name;
+    }
+}
---
+++ src/com/c0dethink/usedGetterClass.java
@@ -0,0 +1,12 @@
+package com.c0dethink;
+
+public class usedGetterClass {
+    public static void main(String[] args) {
+        Getter g1 = new Getter();
+        g1.SetName("first");
+        System.out.println(g1.GetName());
+
+        Getter g2 = new Getter();
+        g2.name
+    }
+}
