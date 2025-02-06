8d4cf96f0680549078daa6019d90c2625480efc6
c0dethink <c0de-think@protonmail.com>
2025-02-06 17:58:47 +0800

    Constructor

--- src/com/c0dethink/Getter.java
+++ src/com/c0dethink/Getter.java
@@ -3,6 +3,11 @@ package com.c0dethink;
 public class Getter {
     private String name;
 
+    public Getter(String name)
+    {
+        this.name = name;
+    }
+
     public void SetName(String name) {
         this.name = name;
     }
