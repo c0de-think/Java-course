b08b2ab64f909a92811e405837d313be4fcd4314
c0dethink <c0de-think@protonmail.com>
2025-02-06 17:13:44 +0800

    class with setter

---
+++ src/com/c0dethink/runMain.java
@@ -0,0 +1,13 @@
+package com.c0dethink;
+
+public class runMain {
+    public static void main(String[] args) {
+        usedClass c1 = new usedClass();
+        c1.setclassName("c0dethink");
+        c1.output();
+
+        usedClass c2 = new usedClass();
+        c2.setclassName("Jack");
+        c2.output();
+    }
+}
---
+++ src/com/c0dethink/usedClass.java
@@ -0,0 +1,13 @@
+package com.c0dethink;
+
+public class usedClass {
+    private String className;
+
+    public void setclassName(String className) {
+        this.className = className;
+    }
+
+    public void output() {
+        System.out.println(className + " " + "say hello");
+    }
+}
